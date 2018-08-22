1cb77e3158aeb02c0ee66e894afdb15bbe6ba5e2
dudua <duduavinoam@gmail.com>
2018-08-22 23:16:48 +0300

    test commit

--- test10/src/pages/home/home.ts
+++ test10/src/pages/home/home.ts
@@ -1,30 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {TestService,TestModel} from '../../app/services/service'
-import { Observable ,Operator } from 'rxjs';
+import { NavController, DateTime } from 'ionic-angular';
+import { TestService, TestModel } from '../../app/services/service'
+import { Observable, Operator } from 'rxjs';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+
 export class HomePage {
 
-  //testList$: Observable<TestModel[]>;
+  dateTimeTest: Date;
+  sunday:string;
+  monday:string;
+  tuesday:string;
+  Wednesday:string;
+  thursday:string;
+  friday:string;
+  saturday:string;
+  testList: TestModel[];
+  
+  today: Date
+  todayString: string;
 
-  addToFire(){
+  addToFire() {
+    console.log(this.todayString);
     this.test.addToDb();
-  console.log("hello from button");
+    console.log("hello from button");
   }
-  showAll(){
-     this.test.getAll().valueChanges().subscribe((datas) => { 
-       
+  showAll() {
+    this.test.getAll().valueChanges().subscribe((datas) => {
+      this.testList = datas;
       console.log("datas", datas)
-    },(err)=>{
-       console.log("probleme : ", err)
+    }, (err) => {
+      console.log("probleme : ", err)
     });
   }
-  constructor(public navCtrl: NavController, private  test :TestService) {
+  constructor(public navCtrl: NavController, private test: TestService) {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+    let yyyy = today.getFullYear();
+    let day = dd.toString();
+    let month = mm.toString(); 
 
+    if (dd < 10) {
+      day = '0' + dd
+    }
+
+    if (mm < 10) {
+      month = '0' + mm
+    }
+
+    this.todayString = yyyy + '-' + month + '-' + day;
+    console.log('fgfg' + this.todayString);
   }
 
 }
